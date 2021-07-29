c23f8a48b9077530d58ce6ac028ae4263cc1f9db
diogopotes <52168413+diogopotes@users.noreply.github.com>
2021-07-29 19:15:14 +0100

    update

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,6 +2,30 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const GameScreen = () => {
+  /*   useEffect(() => {
+
+    const getData = async () => {
+
+            try {
+
+                const {data} = await axios.get('data/data.json');
+
+                setListEmployees(data.employees);
+
+            } catch(err) {
+
+                setError(true);
+                console.log(err.message);
+
+            }
+
+    }
+
+   
+    getData();
+
+},[]) */
+
   return (
     <View>
       <Text>GAME SCREEN</Text>
